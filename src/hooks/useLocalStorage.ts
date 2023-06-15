3a169b356db0c95c002838d8993e1eb97a992a2e
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-06-15 11:53:55 +0300

    feat: add local storage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,23 @@
-const useLocalStorage = () => {
+import {useEffect, useState} from "react";
 
+const useLocalStorage = (key: string, initialValue: any) => {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    // Retrieve the value from local storage
+    const storedValue = localStorage.getItem(key);
+
+    if (storedValue) {
+      setValue(JSON.parse(storedValue));
+    }
+  }, [key]);
+
+  useEffect(() => {
+    // Update local storage when the value changes
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
 }
 
 export {
